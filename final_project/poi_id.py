8af338a913ab9ba5fda195046c8d677eb8fb8eb9
ndvo2710 <kevinvo2710@gmail.com>
2017-04-24 19:50:40 -0700

    Updated

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -28,7 +28,24 @@ with open("final_project_dataset.pkl", "r") as data_file:
 data_dict.pop("TOTAL")
 data_dict.pop("THE TRAVEL AGENCY IN THE PARK")
 
+data_dict['BELFER ROBERT']['deferral_payments'] = 0
+data_dict['BELFER ROBERT']['deferred_income'] = -102500
+data_dict['BELFER ROBERT']['expenses'] = 3285
+data_dict['BELFER ROBERT']['director_fees'] = 102500
+data_dict['BELFER ROBERT']['total_payments'] = 3285
+data_dict['BHATNAGAR SANJAY']['director_fees'] = 0
+data_dict['BHATNAGAR SANJAY']['other'] = 0
+data_dict['BHATNAGAR SANJAY']['expenses'] = 137864
+data_dict['BHATNAGAR SANJAY']['total_payments'] = 137864
 
+data_dict['BELFER ROBERT']['restricted_stock'] = 44093
+data_dict['BELFER ROBERT']['restricted_stock_deferred'] = -44093
+data_dict['BELFER ROBERT']['exercised_stock_options'] = 0
+data_dict['BELFER ROBERT']['total_stock_value'] = 0
+data_dict['BHATNAGAR SANJAY']['restricted_stock'] = 2604490
+data_dict['BHATNAGAR SANJAY']['restricted_stock_deferred'] = -2604490
+data_dict['BHATNAGAR SANJAY']['exercised_stock_options'] = 15456290
+data_dict['BHATNAGAR SANJAY']['total_stock_value'] = 15456290
 
 ### Task 3: Create new feature(s)
 # Bonus-salary ratio
@@ -126,6 +143,8 @@ sss = StratifiedShuffleSplit(labels, 100, test_size=0.3, random_state=60)
 ###################################
 ####### K Best + Naive Bayes
 ###################################
+pipeline1 = Pipeline([("kbest", skb), ("nb", nb )])
+params1 = {"kbest__k": range(5, 10)}
 gs = GridSearchCV(pipeline1, params1, n_jobs=-1, cv=sss)
 gs.fit(features, labels)
 
@@ -147,6 +166,45 @@ print "The features used are:", feature_names
 ###################################
 ####### K-Best + Decision Tree:
 ###################################
+pipeline2 = Pipeline([("kbest", skb), ("dt", dt )])
+params2 = {"kbest__k": range(5, 10),
+           "dt__min_samples_split": range(2,10),
+           "dt__min_samples_leaf": range(2,10),
+           "dt__criterion": ["gini", "entropy"]}
+
+gs = GridSearchCV(pipeline2, params2, n_jobs=-1, cv=sss, scoring="f1")
+gs.fit(features, labels)
+clf= gs.best_estimator_
+print "Tester Classification report"
+test_classifier(clf.named_steps["dt"], data_dict, features_list)
+
+features_used = gs.best_estimator_.named_steps["kbest"].get_support(indices=True)
+print "A total of %d features were used" % len(features_used)
+#Note 1: You use 'features_list[i+1]', instead of 'features_list[i]',
+#because the first feature in that list is 'poi'
+#which you didn't include in the variable 'features'
+feature_names = [features_list[i + 1] for i in features_used]
+#print "The features used are:", feature_names
+scores = [clf.named_steps['kbest'].scores_[i + 1] for i in features_used]
+#print 'Scores: ', scores
+importances = [clf.named_steps['dt'].feature_importances_[i+1] for i in features_used]
+#print 'Importance: ', importances
+import numpy as np
+indices = np.argsort(importances)[::-1]
+#print 'Indices: ', indices
+for i in range(len(feature_names)):
+    print "feature no. {}: {} ({}) ({})".format(i+1, feature_names[indices[i]], importances[indices[i]], scores[indices[i]])
+
+
+
+from sklearn.preprocessing import MinMaxScaler
+scale = MinMaxScaler()
+pipeline2 = Pipeline([("scale", scale), ("kbest", skb), ("dt", dt )])
+params2 = {"kbest__k": range(5, 10),
+           "dt__min_samples_split": range(2,10),
+           "dt__min_samples_leaf": range(2,10),
+           "dt__criterion": ["gini", "entropy"]}
+
 gs = GridSearchCV(pipeline2, params2, n_jobs=-1, cv=sss, scoring="f1")
 gs.fit(features, labels)
 clf= gs.best_estimator_
@@ -175,6 +233,12 @@ for i in range(len(feature_names)):
 ####### K-Best + PCA + Decision Tree:
 ###################################
 
+pipeline4 = Pipeline([("kbest", skb), ("PCA", pca ),("dt", dt )])
+params4 = {"kbest__k": range(5, 10),
+           "PCA__whiten": [True, False],
+           "dt__min_samples_split": [2, 4, 6],
+           "dt__min_samples_leaf": [2, 4, 6],
+           "dt__criterion": ["gini", "entropy"]}
 gs = GridSearchCV(pipeline4, params4, n_jobs=-1, cv=sss, scoring="f1")
 gs.fit(features, labels)
 clf= gs.best_estimator_
@@ -203,6 +267,10 @@ for i in range(len(feature_names)):
 ####### K-Best + Random Forest:
 ###################################
 
+pipeline3 = Pipeline([("kbest", skb), ("rf", rf )])
+params3 = {"kbest__k": range(5, 10),
+           "rf__max_depth": [None, 5, 10],
+           "rf__n_estimators": [10, 15, 20]}
 gs = GridSearchCV(pipeline3, params3, n_jobs=-1, cv=sss, scoring="f1")
 gs.fit(features, labels)
 clf= gs.best_estimator_
@@ -224,7 +292,6 @@ indices = np.argsort(importances)[::-1]
 #print 'Indices: ', indices
 for i in range(len(feature_names)):
     print "feature no. {}: {} ({}) ({})".format(i+1, feature_names[indices[i]], importances[indices[i]], scores[indices[i]])
-
 
 
 
